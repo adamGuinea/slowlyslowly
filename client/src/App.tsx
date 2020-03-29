44a9c9f1686c4a514e05f901c996f7f4bf341981
adamGuinea <adamguinea22@gmail.com>
2020-03-29 12:49:31 +1000

    adding client

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <Fragment>
+      <div>my app</div>
+    </Fragment>
+  );
+}
+
+export default App;
